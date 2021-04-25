86a175137751cf96e593825aca4cdbc00a2ce8c5
am <ab2qik@gmail.com>
2021-04-25 19:55:51 +0300

    small change

--- src/pages/Journal.vue
+++ src/pages/Journal.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout>
-
     <div class="container">
       <div class="journal-hero">
         <h1 class="journal-header">
@@ -8,7 +7,6 @@
         </h1>
       </div>
     </div>
-
     <g-link 
       :to="item.node.path"
       v-for="item in $page.posts.edges" 
@@ -20,10 +18,8 @@
         <p class="journal-excerpt">{{ item.node.excerpt }}</p>
       </div>
     </g-link>
-      
   </Layout>
 </template>
-
 <page-query>
 query Journal {
 	posts: allJournalPost {
@@ -38,64 +34,61 @@ query Journal {
   }
 }
 </page-query>
-
 <script>
-export default {
-}
+  export default {}
 </script>
-
 <style scoped>
-.container.journal {
-  max-width: 720px;
-}
-.journal-hero {
-  padding: 4rem 0;
-  text-align: center;
-  color: var(--color-base-1);
-}
-.journal-header {
-  font-size: 3rem;
-  font-weight: 700;
-  padding: 0;
-  margin: 0;
-}
-.journal-post {
-  display: block;
-  padding: 2rem 0;
-  text-decoration: none;
-  transition: background 0.5s ease;
-}
-.journal-post > * {
-  transition: transform 0.5s ease;
-}
-.journal-post:hover {
-  background-color: var(--color-base-1);
-}
-.journal-post:hover > * {
-  transform: translateX(4rem);
-}
-.journal-post h1,
-.journal-post h2 {
-  margin: 0;
-  padding: 0;
-}
-.journal-title {
-  font-size: 2rem;
-  color: var(--color-contrast);
-}
-.journal-excerpt {
-  color: var(--color-contrast-1);
-}
-
-@media (min-width: 560px) {
-  .journal-post {
-    padding: 3rem 0;
+  .container.journal {
+    max-width: 720px;
   }
-}
-
-@media (min-width: 860px) {
-  .journal-post {
-    padding: 5rem 0;
+  .journal-hero {
+    padding: 4rem 0;
+    text-align: center;
+    color: var(--color-base-1);
   }
-}
+  .journal-header {
+    font-size: 3rem;
+    font-weight: 700;
+    padding: 0;
+    margin: 0;
+  }
+  .journal-post {
+    display: block;
+    padding: 2rem 0;
+    text-decoration: none;
+    transition: background 0.5s ease;
+  }
+  .journal-post > * {
+    transition: transform 0.5s ease;
+  }
+  .journal-post:hover {
+    background-color: var(--color-base-1);
+  }
+  .journal-post:hover > * {
+    transform: translateX(4rem);
+  }
+  .journal-post h1,
+  .journal-post h2 {
+    margin: 0;
+    padding: 0;
+  }
+  .journal-title {
+    font-size: 2rem;
+    color: var(--color-contrast);
+  }
+  .journal-excerpt {
+    color: var(--color-contrast-1);
+  }
+
+  @media (min-width: 560px) {
+    .journal-post {
+      padding: 3rem 0;
+    }
+  }
+
+  @media (min-width: 860px) {
+    .journal-post {
+      padding: 5rem 0;
+    }
+  }
 </style>
